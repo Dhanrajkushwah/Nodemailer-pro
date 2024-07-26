3158cc98966b42c6bd0e948b16df895fbf648a60
Dhanraj Kushwah <dhanraj.kushwah007@gmail.com>
2024-07-26 17:27:24 +0530

    First1

--- emailService.js
+++ emailService.js
@@ -18,8 +18,8 @@ const sendMail = async (to, subject, text) => {
       from: `"Dhanraj" <${process.env.EMAIL_USER}>`, // sender address
       to: "dhanraj.kushwah007@gmail.com",
       subject: "Email Task", // Subject line
-      text: "Hello How are you", // plain text body
-     
+      text: "Hello How are you. Nodemailer is a module for Node.js applications to allow easy as cake email sending", // plain text body
+      html: "<b>Hello world?</b>", 
     });
 
     console.log('Message sent: %s', info.messageId);
